24b128beeab986baa0924abc03ada796ca209d6b
fullstackmechanic <sirhmvfx@gmail.com>
2025-01-03 10:53:42 +0100

    refactor blog card for responsiveness

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -20,10 +20,10 @@ function BlogCard({ title, image, description, link }: Blog) {
       </div>
       <div className="md:w-1/2 flex flex-col justify-between items-start ">
         <div className="flex flex-col gap-2 items-start py-2">
-          <h1 className="md:text-[20px] text-xl leading-tight font-medium">{title}</h1>
-          <p className="text-gray-500 text-sm md:text-sm">{description.split(" ").slice(0, 10).join(" ")}.....</p>
+          <h1 className="md:text-[20px] text-[13px] leading-tight font-medium">{title}</h1>
+          <p className="text-gray-500 md:text-base text-[10px]">{description.split(" ").slice(0, 10).join(" ")}.....</p>
         </div>
-        <Link href={link} className=" text-primarycolor underline rounded-md">
+        <Link href={link} className=" text-primarycolor  underline rounded-md">
           Read More
         </Link>
       </div>
