8c793fd6debbd9a5c05b6affc3e04e933f9355bb
fullstackmechanic <sirhmvfx@gmail.com>
2025-01-06 18:36:31 +0100

    removed email and contatc repitition

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,3 @@
-import { BiMailSend, BiPhone } from "react-icons/bi";
 import { BsArrowRight } from "react-icons/bs";
 
 export default function Contact() {
@@ -9,9 +8,9 @@ export default function Contact() {
       <p className="text-gray-500 text-base md:text-lg md:w-1/2">Have questions, suggestions, or want to partner with us? Reach out today and let’s work together to make a difference!</p>
     </div>
 
-    <div className="md:w-5/6 mx-auto py-16 flex md:flex-row flex-col items-center gap-4">
+    <div className="md:w-5/6 mx-auto py-4 flex md:flex-row flex-col items-center gap-4">
       <div className="w-full flex md:flex-row flex-col gap-8 p-6 md:p-16 bg-gray-50 rounded-lg">
-        <form className="md:w-3/5 flex flex-col gap-4 items-start">
+        <form className="w-full flex flex-col gap-4 items-start">
         <h1 className="text-xl md:text-3xl lg:text-4xl font-medium">Leave us your info </h1>
         <input type="text" className="w-full bg-transparent p-4 rounded-md border border-gray-300" placeholder="Your Name" />
         <input type="text" className="w-full bg-transparent p-4 rounded-md border border-gray-300" placeholder="Your Email" />
@@ -26,32 +25,7 @@ export default function Contact() {
         </button>
         
         </form>
-        <div className="md:w-2/5 md:p-10 p-4 bg-white flex flex-col gap-8 rounded-2xl">
-          <div className="flex items-start gap-4">
-            <div className=" hidden md:block p-2 border border-primarycolor rounded-full">
-            <BiMailSend className="text-2xl md:text-3xl lg:text-4xl w-6 h-6 text-primarycolor " />
-              
-            </div>
-          <div className="flex flex-col gap-2 items-start">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-medium">Send us an Email</h1>
-            <p className="text-gray-500 text-base md:text-lg">We&apos;d love to hear from you! Please feel free to reach out to us via mail.</p>
-            <button className="bg-primarycolor text-white p-2 rounded-md">info@commonpersonrelief.com</button>
-          </div>
-          </div>
-
-          <div className="w-full h-[1px] bg-gray-300"></div>
-          <div className="flex items-start gap-4">
-            <div className="hidden md:block p-2 border border-primarycolor rounded-full">
-            <BiPhone className="text-2xl md:text-3xl lg:text-4xl w-6 h-6 text-primarycolor " />
-              
-            </div>
-          <div className="flex flex-col gap-2 items-start">
-            <h1 className="text-xl md:text-2xl lg:text-3xl font-medium">Give us a call </h1>
-            <p className="text-gray-500 text-base md:text-lg">We&apos;re here to help! Don&apos;t hesitate to reach out to us by phone.</p>
-            <button className="bg-primarycolor text-white p-2 rounded-md">+1 (432) 803-1218</button>
-          </div>
-          </div>
-        </div>
+       
       </div>
     </div>
   </div>;
