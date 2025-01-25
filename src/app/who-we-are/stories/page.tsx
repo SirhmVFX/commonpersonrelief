b6bdc1ffa4ef785808bec4c7d64978186b5f134f
fullstackmechanic <sirhmvfx@gmail.com>
2025-01-25 10:58:41 +0100

    remove blogs from here to data file

--- src/app/who-we-are/stories/page.tsx
+++ src/app/who-we-are/stories/page.tsx
@@ -1,59 +1,12 @@
+import { blogs } from "@/app/data";
 import BlogCard from "@/components/BlogCard";
 import Link from "next/link";
 import { BiArrowBack } from "react-icons/bi";
 
-interface Blog {
-  id: number;
-  title: string;
-  image: string;
-  link: string;
-  description: string;
-}
+
 
 export default function Stories() {
-  const blogs: Blog[] = [
-    {
-      id: 1,
-      title:
-        " January 2025 Events: Empowering Communities Through Outreach in the state of Ibadan",
-      image: "/images/rec.png",
-      link: "/who-we-are/stories/january-2025-outreach",
-      description: "Together, we bring hope and relief to those in need.",
-    },
-    {
-      id: 2,
-      title: " How Your Small Donations Make a Big Impact",
-      image: "/images/at1.jpg",
-      link: "/who-we-are/stories/how-your-small-donations-make-a-big-impact",
-      description:
-        "When it comes to charity, many people think that only large donations make a difference. However, the truth is that small contributions collectively have the power to transform lives and uplift communities",
-    },
-    {
-      id: 3,
-      title: "The Role of Education in Breaking the Cycle of Poverty",
-      image: "/images/at2.jpg",
-      link: "/who-we-are/stories/the-role-of-education-in-breaking-the-cycle-of-poverty",
-      description:
-        "Education is more than just learning—it’s a pathway to a brighter future.",
-    },
-    {
-      id: 4,
-      title:
-        " How Clean Water Access Transforms Lives in Underserved Communities",
-      image: "/images/at3.jpg",
-      link: "/who-we-are/stories/how-clean-water-access-transforms-lives-in-underserved-communities",
-      description:
-        "Our recent food drive provided over 5,000 meals to underserved communities, ensuring families have the nourishment they need. Join us for next month's drive!",
-    },
-    {
-      id: 5,
-      title: "Spreading Smiles: The Joy of Giving in Unique Ways",
-      image: "/images/at4.jpg",
-      link: "/who-we-are/stories/spreading-smiles-the-joy-of-giving-in-unique-ways",
-      description:
-        "We've plan to launched a new shelter initiative to provide safe, temporary housing for displaced individuals. Together, we're creating spaces of hope and recovery.",
-    },
-  ];
+ 
   return (
     <div>
       <div className="w-5/6 mx-auto py-20 md:pt-32 md:flex items-center gap-16">
