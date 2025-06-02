b7b743b54e83f4588865abef5b0901bc49aafa1c
sirhmvfx <sirhmvfx@gmail.com>
2025-06-02 15:22:40 +0100

    uodate layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Header2 from "@/components/Header2";
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${plusJakarta.variable} antialiased`}>
-        <Header />
+        <Header2 />
 
         {children}
         <Footer />
