c0f96ac537322137bfbf1e6308ac7d287aff4d6e
sirhmvfx <sirhmvfx@gmail.com>
2025-06-02 15:23:54 +0100

    modified blog card

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,19 +8,19 @@ interface Blog {
 }
 function BlogCard({ title, image, description, link }: Blog) {
   return (
-    <div className="md:flex gap-14">
-      <div className="md:w-1/2 h-[200px]">
+    <div className="md:flex gap-6">
+      <div className="md:w-1/2 h-[150px]">
         <Image
           src={image}
           alt="news"
           width={1000}
           height={1000}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover rounded-xl"
         />
       </div>
       <div className="md:w-1/2 flex flex-col justify-between items-start ">
         <div className="flex flex-col gap-2 items-start py-2">
-          <h1 className="md:text-[20px] text-[13px] leading-tight font-medium">{title}</h1>
+          <h1 className="md:text-[16px] text-[13px] leading-tight font-medium">{title}</h1>
           <p className="text-gray-500 md:text-base text-[10px]">{description.split(" ").slice(0, 10).join(" ")}.....</p>
         </div>
         <Link href={link} className=" text-primarycolor  underline rounded-md">
