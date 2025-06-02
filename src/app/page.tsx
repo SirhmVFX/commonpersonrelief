1d446049c2921e1f1e9f80b8dcc82300c207210c
sirhmvfx <sirhmvfx@gmail.com>
2025-06-02 15:22:46 +0100

    fix page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import Image from "next/image";
 import BlogCard from "@/components/BlogCard";
 import CtaPage from "@/components/Cta.page";
-import Herosection from "@/components/Herosection";
 import Link from "next/link";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { blogs } from "./data";
+import Herosection2 from "@/components/Herosection2";
+import Button from "@/components/Button";
 
 
 
@@ -28,37 +29,34 @@ export default function Home() {
 
   return (
     <>
-      <Herosection />
+      <Herosection2 />
       <div className="md:w-5/6 h-screen md:h-[80vh] mx-auto p-4 md:py-[40px] lg:pt-[120px] flex flex-col justify-center gap-4">
         <div className="flex  gap-4">
           <div className="w-full flex flex-col md:flex-row gap-10 items-center ">
-            <div className="w-full h-[300px] md:h-[400px] lg:h-[500px]">
+            <div className="w-2/5 h-[300px] md:h-[300px] lg:h-[400px]">
               <Image
                 src="/images/wee.png"
                 alt="about"
                 width={1000}
                 height={1000}
-                className="w-full h-full object-cover rounded-md"
+                className="w-full h-full object-cover rounded-xl"
               />
             </div>
-            <div className="flex flex-col gap-4 items-start ">
+            <div className="flex flex-col gap-4 items-start w-3/5 ">
               <p className="text-gray-500">About us</p>
-              <h1 className="text-4xl md:text-5xl lg:text-7xl font-medium">
+              <h1 className="text-xl md:text-3xl lg:text-6xl font-medium">
                 Who we are
               </h1>
 
-              <p className="text-gray-500">
+              <p className="text-gray-500 text-base md:text-xl">
                 Common Person Relief is a nonprofit organization committed to
                 helping the underserved with food, shelter, clothing, and other
                 basic needs. We believe in restoring dignity and building
                 stronger communities, one life at a time
               </p>
-              <Link
-                href={"/who-we-are"}
-                className=" sm:w-auto bg-primarycolor text-white p-3 rounded-md"
-              >
-                Learn More
-              </Link>
+              <Button type="link" url="/who-we-are" title="Learn More" classname="bg-primarycolor text-white"/>
+
+              
             </div>
           </div>
         </div>
@@ -66,7 +64,7 @@ export default function Home() {
 
       <div className="md:w-5/6 h-screen mx-auto md:py-10 p-4 lg:pt-32">
         <div className=" flex flex-col gap-4">
-          <h1 className="text-4xl md:text-5xl lg:text-7xl font-medium">
+          <h1 className="text-xl md:text-3xl lg:text-5xl font-medium">
             Our Programs
           </h1>
 
@@ -174,29 +172,29 @@ export default function Home() {
         </div>
       </div> */}
 
-      <div className="md:w-5/6 mx-auto p-4 h-[70vh] my-10">
-      <h1 className="text-4xl md:text-5xl lg:text-7xl font-medium my-4">
+      <div className="md:w-5/6 mx-auto p-4 h-[60vh] my-10 flex flex-col gap-6">
+      <h1 className="text-xl md:text-3xl lg:text-5xl font-medium my-4 ">
             Upcoming event
           </h1>
         <div className="w-full h-full relative">
           <Image
-            src={"/images/rec.png"}
+            src={"/images/wort1.jpg"}
             width={1000}
             height={1000}
             alt="event"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-xl"
           />
-          <div className="absolute top-0 left-0 right-0 bottom-0 from-black to-transparent  bg-gradient-to-t  "></div>
-          <div className="absolute left-10 bottom-10 right-10 flex flex-col items-start  gap-4">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-medium text-white">
-            January 2025 Events: Empowering Communities Through Outreach in
-            the state of Ibadan
+          <div className="absolute top-0 left-0 right-0 bottom-0 from-black to-transparent  bg-gradient-to-t rounded-xl  "></div>
+          <div className="absolute left-10 bottom-10 right-10 flex flex-col items-start  gap-2">
+            <h1 className="text-md md:text-xl lg:text-4xl font-medium text-white">
+            
+Work Ready Transition Program (WORT)
             </h1>
             <p className="text-white">
             Together, we bring hope and relief to those in need.</p>
             <Link
               href="/who-we-are/stories/january-2025-outreach"
-              className=" sm:w-auto text-primarycolor flex  gap-4 items-center border border-primarycolor p-3 rounded-full"
+              className=" sm:w-auto text-primarycolor flex  gap-4 items-center"
             >
               Learn More
               <AiOutlineArrowRight className="w-[30px] h-[30px] p-2 rounded-full border border-white" />
@@ -205,15 +203,30 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="md:w-5/6 justify-center mx-auto md:py-10 p-4 lg:py-20 flex flex-col gap-4">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-medium">
-          Read our latest events and news
+      <div className="md:w-5/6 justify-center mx-auto  flex flex-col gap-2 md:mt-32">
+        <h1 className="text-lg md:text-2xl lg:text-4xl font-medium">
+          Read our Latest Events and News
         </h1>
-        <div className="grid grid-cols-2 md:grid-cols-2 gap-8 lg:gap-20 pt-10">
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-8 lg:gap-10 pt-10">
           {blogs.slice(0, 4).map((blog) => (
             <BlogCard key={blog.id} {...blog} />
           ))}
         </div>
+
+        <Link href={"/who-we-are/stories"} className="text-center p-8">See More</Link>
+      </div>
+
+
+      <div className="md:w-5/6 justify-center mx-auto flex flex-col gap-2 md:mt-20">
+        <h1 className="text-lg md:text-2xl lg:text-4xl font-medium">
+          Our Blogs and Articles
+        </h1>
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-8 lg:gap-10 pt-10 pb-10">
+          {blogs.slice(4, 8).map((blog) => (
+            <BlogCard key={blog.id} {...blog} />
+          ))}
+        </div>
+        <Link href={"/who-we-are/stories"} className="text-center p-8">See More</Link>
       </div>
       <CtaPage />
     </>
