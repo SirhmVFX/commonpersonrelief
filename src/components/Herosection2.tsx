a2f37dd01129a9537982c5b21dd6e3c0801040f7
sirhmvfx <sirhmvfx@gmail.com>
2025-06-03 03:22:44 +0100

    modified images to cdn images

--- src/components/Herosection2.tsx
+++ src/components/Herosection2.tsx
@@ -5,7 +5,7 @@ const Herosection2 = () => {
     return (
         <div className="h-screen relative">
 
-            <Image src={"/images/whero.png"} width={10000} height={10000} alt="img" className="w-full h-full object-cover absolute" />
+            <Image src={"https://res.cloudinary.com/dkeh0cumc/image/upload/v1748879242/whero_jp2teh.png"} width={10000} height={10000} alt="img" className="w-full h-full object-cover absolute" />
             <div className="absolute top-0 bottom left-0 right-0 h-full flex flex-col items-center pt-32 gap-4 ">
 
                 <h1 className="text-white agr leading-[60px] text-[61px] text-center w-4/6">Empowering Vulnerable Communities Across Africa, One Step at a Time.</h1>
